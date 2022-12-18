7f576c1cf912a04f705d673bc1790c1593746c31
Hussien El-Sawy <hussien.elsawy@outlook.com>
2022-12-18 18:47:10 +0200

    deleted test files

--- webapp.py
+++ webapp.py
@@ -3,7 +3,7 @@ from modules.elastic_connector import *
 from modules.mrelk import *
 
 app = Flask(__name__)
-
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 @app.route('/')
 def home():
@@ -48,19 +48,30 @@ def nodes():
                                         config['elasticsearch.password'], 
                                         config['elasticsearch.ssl.certificateAuthorities'], 
                                         config['elasticsearch.ssl.verificationMode'])
-    node_stats_headers, node_stats_data, master_id = get_node_stats(es)
-    allocation_headers, allocation_data = get_allocation(es)
-    thread_pool_headers, thread_pool_data = get_thread_pool(es)
-
-    return render_template('Nodes.html', 
-                connection_status=connection_status,
-                node_stats_headers=node_stats_headers,
-                node_stats_data=node_stats_data,
-                master_id=master_id,
-                allocation_headers=allocation_headers,
-                allocation_data=allocation_data,
-                thread_pool_headers=thread_pool_headers,
-                thread_pool_data=thread_pool_data)
+    if connection_status == "Connected":
+        node_stats_headers, node_stats_data, master_id = get_node_stats(es)
+        allocation_headers, allocation_data = get_allocation(es)
+        thread_pool_headers, thread_pool_data = get_thread_pool(es)
+        return render_template('Nodes.html', 
+                    connection_status=connection_status,
+                    node_stats_headers=node_stats_headers,
+                    node_stats_data=node_stats_data,
+                    master_id=master_id,
+                    allocation_headers=allocation_headers,
+                    allocation_data=allocation_data,
+                    thread_pool_headers=thread_pool_headers,
+                    thread_pool_data=thread_pool_data)
+    else:
+        cluster_status = ""
+        active_engines = ""
+        watchers_status = ""
+        inactive_engines = ""
+        return render_template('index.html', 
+                connection_status=connection_status, 
+                cluster_status=cluster_status,
+                active_engines=active_engines,
+                inactive_engines=inactive_engines,
+                watchers_status=watchers_status)
 
 if __name__ == '__main__':
     arguments, arg_valid = validate_arugments(sys.argv)
